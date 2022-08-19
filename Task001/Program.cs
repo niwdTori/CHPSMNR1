51e6fbd8f7fbd1553a410f2f5f977decb86b5096
Tori <mes.viktoria@mail.ru>
2022-08-19 19:07:19 +0500

    Является ли первое число квадратом второго?

---
+++ Task001/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Hello! Is the first number the square of the second?");
+
+Console.WriteLine("Please, input first number: ");
+string FNumber = Console.ReadLine();
+int FirstNumber = int.Parse(FNumber);
+
+Console.WriteLine("Please, input second number: ");
+string SNumber = Console.ReadLine();
+int SecondNumber = int.Parse(SNumber);
+
+if(SecondNumber * SecondNumber == FirstNumber) 
+{
+    Console.WriteLine("Yes!");
+}
+else 
+{
+    Console.WriteLine("No:(");
+}
