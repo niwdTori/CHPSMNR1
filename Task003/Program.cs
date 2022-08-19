9a2cc51ae2aa2b57c69f1a54c7f162e1154de185
Tori <mes.viktoria@mail.ru>
2022-08-19 20:44:15 +0500

    All integers from -N to N

---
+++ Task003/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Hello! All integers from -N to N");
+Console.WriteLine("Please, input number: ");
+int numberA = int.Parse(Console.ReadLine());
+
+for (int numberB = (-numberA); numberB <= numberA; numberB++)
+    Console.Write(numberB + " ");
