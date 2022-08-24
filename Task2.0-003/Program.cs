fef09cb01324173404b765fa9e6c437569b71e63
Tori <mes.viktoria@mail.ru>
2022-08-25 00:21:57 +0500

    Кратно ли первое число второму

---
+++ Task2.0-003/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+int number = num1 % num2;
+if(number == 0) {Console.WriteLine("Кратно");}
+else {Console.WriteLine($"Не кратно, остаток {number}");}
+
