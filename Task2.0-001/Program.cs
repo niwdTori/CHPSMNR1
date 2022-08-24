868becfa70a21a7a121246641784183f78f3297a
Tori <mes.viktoria@mail.ru>
2022-08-24 23:31:17 +0500

    Максимальная цифра в числе от 1 до 10

---
+++ Task2.0-001/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = new Random().Next(10, 100);
+int a1 = number/ 10;
+int a2 = number % 10;
+
+int max = a1;
+if (max < a2){ max = a2;}
+
+Console.WriteLine($"В числе {number} максимальная цифра: {max}");
