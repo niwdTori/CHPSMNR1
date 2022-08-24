5a4922cbb6da04c7134df42f57420d1f3da2307c
Tori <mes.viktoria@mail.ru>
2022-08-25 00:00:27 +0500

    Удаление 2 числа из трёхзначного

---
+++ Task2.0-002/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = new Random().Next(100, 1000);
+int a1 = number / 100 * 10;
+int a3 = number % 10;
+
+int num = a1 + a3;
+
+Console.WriteLine($"a1: {a1} , a3: {a3}");
+Console.WriteLine($"Число: {number} -> {num}");
