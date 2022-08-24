7a6d43e908e0fb74a87ac272e92e0a30fd12ea95
Tori <mes.viktoria@mail.ru>
2022-08-25 01:19:32 +0500

    Квадрат чисел

---
+++ Task2.0-005/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе чило: ");
+int num2 = int.Parse(Console.ReadLine());
+
+int max = num2;
+int min = num1;
+
+if (num1 > num2)
+{
+    max = num1;
+    min = num2;
+}
+
+if( min * min == max) 
+{
+    Console.WriteLine("Да");
+}
+else 
+{
+    Console.WriteLine("Нет");
+}
