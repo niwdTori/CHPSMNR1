4742a8dc72c0295e8ae0473dba489bf0a8070f21
Tori <mes.viktoria@mail.ru>
2022-08-27 22:51:44 +0500

    .\Task3.0-002\

--- Task3.0-001/Program.cs
+++ Task3.0-001/Program.cs
@@ -1,10 +1,15 @@
-﻿Console.WriteLine("Пожалуйста, введите х: ");
+﻿//void VivestiChetvert(int chetvert) 
+// {
+//     Console.WriteLine($"Точка принадлежит {chetvert}");
+// }
+
+Console.WriteLine("Пожалуйста, введите х: ");
 int x = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Пожалуйста, введите y: ");
 int y = int.Parse(Console.ReadLine());
 
-if (x > 0 && y > 0) { Console.WriteLine("Точка находится в 1 четверти");}
-if (x > 0 && y < 0) { Console.WriteLine("Точка находится в 4 четверти");}
-if (x < 0 && y > 0) { Console.WriteLine("Точка находится в 2 четверти");}
-if (x < 0 && y < 0) { Console.WriteLine("Точка находится в 3 четверти");}
+if (x > 0 && y > 0) { Console.WriteLine("Точка находится в 1 четверти"); }
+if (x > 0 && y < 0) { Console.WriteLine("Точка находится в 4 четверти"); }
+if (x < 0 && y > 0) { Console.WriteLine("Точка находится в 2 четверти"); }
+if (x < 0 && y < 0) { Console.WriteLine("Точка находится в 3 четверти"); }
