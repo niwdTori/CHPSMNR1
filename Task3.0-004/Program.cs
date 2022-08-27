3a46c821fe42dceefa5a9d2886d5f847991989c3
Tori <mes.viktoria@mail.ru>
2022-08-27 23:56:54 +0500

    Таблица квадратов чисел от 1 до n

---
+++ Task3.0-004/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+int b = 1;
+
+while(b <= a)
+{
+    double c = Math.Pow(b, 2);
+    Console.Write(c + ", ");
+    b++;
+}
