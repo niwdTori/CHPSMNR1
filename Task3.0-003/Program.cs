038b3854c34b3456eee5a6ee3f30a50e33f2eb08
Tori <mes.viktoria@mail.ru>
2022-08-27 23:43:09 +0500

    Расстояние между координатами

---
+++ Task3.0-003/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите x1: ");
+int x1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите x2: ");
+int x2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите y1: ");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите y2: ");
+int y2 = int.Parse(Console.ReadLine());
+
+double A = Math.Pow((x2 - x1), 2);
+double B = Math.Pow((y2 - y1), 2);
+
+
+double AB = Math.Sqrt(A + B);
+
+Console.WriteLine($"Расстояние равно: {AB}");
