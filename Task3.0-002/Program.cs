950e00e7dd5bc63ef1e89fd3a867646ebf15856e
Tori <mes.viktoria@mail.ru>
2022-08-27 22:52:26 +0500

    Диапазон координат

---
+++ Task3.0-002/Program.cs
@@ -0,0 +1,13 @@
+﻿ Console.WriteLine("Пожалуйста, введите номер четверти: ");
+ int number = int.Parse(Console.ReadLine());
+
+while(number < 0 || number > 4) 
+{
+    Console.WriteLine("Пожалуйста, введите номер четверти: ");
+    number = int.Parse(Console.ReadLine());
+}
+
+if(number == 1) {Console.WriteLine("Диапазон возможных координат: x > 0, y > 0");}
+if(number == 2) {Console.WriteLine("Диапазон возможных координат: x < 0, y > 0");}
+if(number == 3) {Console.WriteLine("Диапазон возможных координат: x < 0, y < 0");}
+if(number == 4) {Console.WriteLine("Диапазон возможных координат: x > 0, y < 0");}
