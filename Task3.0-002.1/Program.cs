48d77d05d0c0a358ad053150aec712ee8beebd7f
Tori <mes.viktoria@mail.ru>
2022-08-27 23:13:23 +0500

    Диапазон координат2.0

---
+++ Task3.0-002.1/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите номер четверти: ");
+int number = int.Parse(Console.ReadLine());
+
+switch(number)
+{
+    case 1:
+    {
+        Console.WriteLine("x > 0, y > 0");
+        break;
+    }
+    case 2:
+    {
+        Console.WriteLine("x < 0, y > 0");
+        break;
+    }
+    case 3:
+    {
+        Console.WriteLine("x < 0, y < 0");
+        break;
+    }
+    case 4:
+    {
+        Console.WriteLine("x > 0, y < 0");
+        break;
+    }
+
+    default:
+    {
+        Console.WriteLine("Введено неправильное значение для четверти");
+        break;
+    }
+}
