9e219ae7684d4c316db67006aab8612f292efbf0
Tori <mes.viktoria@mail.ru>
2022-09-01 00:44:14 +0500

    Сколько цифр в числе

---
+++ Task4.0-002/Program.cs
@@ -0,0 +1,15 @@
+﻿void NumberOfDigits(int number)
+{
+    int count = 0;
+    number = Math.Abs(number);
+    while(number > 0)
+    {
+        number = number / 10;
+        count++;
+    }
+    Console.WriteLine(count);
+}
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+NumberOfDigits(a);
