009749c9c6f088c23f9693eec8a4566aba9eaeba
Tori <mes.viktoria@mail.ru>
2022-09-01 00:23:19 +0500

    Сумма чисел от 1 до N

---
+++ Task4.0-001/Program.cs
@@ -0,0 +1,22 @@
+﻿// Console.WriteLine("Введите число: ");
+// int a = int.Parse(Console.ReadLine());
+// int sum = 0;
+// for(int b = 1; b <= a; b++)
+// {
+//    sum = sum + b;
+// }
+// Console.WriteLine(sum);
+
+void GetSumNums(int n)
+{
+    int sum = 0;
+    for(int b = 1; b <= n; b++)
+    {
+        sum += b;
+    }
+    Console.WriteLine(sum);
+}
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+GetSumNums(a);
