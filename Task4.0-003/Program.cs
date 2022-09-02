d16a6d0f29b37ec658ec3cf3fcc2b212adbc439d
Tori <mes.viktoria@mail.ru>
2022-09-02 21:15:27 +0500

    Произведение чисел от 1 до n

---
+++ Task4.0-003/Program.cs
@@ -0,0 +1,16 @@
+﻿//Произведение чисел от 1 до N
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+void MultiplicationfNumbers(int number)
+{
+    int sum = 1;
+    for(int n = 2; n <= number; n++)
+    {
+        sum = sum * n;
+    }
+    Console.WriteLine($"Произведение равно: {sum}");
+}
+
+MultiplicationfNumbers(a);
