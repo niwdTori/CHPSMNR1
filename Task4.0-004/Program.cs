eaf82274c907abfe73909659981887469bc93c95
Tori <mes.viktoria@mail.ru>
2022-09-02 21:30:07 +0500

    Заполнить массив

---
+++ Task4.0-004/Program.cs
@@ -0,0 +1,22 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    for (int index = 0; index < length; index++)
+    {
+        collection[index] = new Random().Next(0,2);
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    for(int position = 0; position < count; position++)
+    {
+        Console.Write($"{col[position]}, ");
+    }
+}
+
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
