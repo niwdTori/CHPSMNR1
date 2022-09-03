d6fea733358fb6e3360b5242fff6ca67cef71c6f
Tori <mes.viktoria@mail.ru>
2022-09-03 23:38:07 +0500

    string.Join

---
+++ Task5.0-001/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] arr = new int[3];
+arr[0] = 1;
+arr[1] = 2;
+arr[2] = 3;
+
+string concat = string.Join(",", arr);
+//Выводит элементы массива через запятую
+string MyJoin(string delimiter, int[] array)
+{
+    string result = ""; //создание пустой строки
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        result = result + array[i] + delimeter;
+    }
+    result = result + array[array.Length - 1];
+
+    return result;
+}
+
+string joinedArray = MyJoin(",", arr);
+Console.WriteLine(joinedArray);
