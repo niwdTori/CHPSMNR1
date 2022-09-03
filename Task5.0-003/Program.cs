706c2a88571ef2b763c6048cb6a276e518b28773
Tori <mes.viktoria@mail.ru>
2022-09-04 01:10:27 +0500

    Замена пол-ых цифр на отриц-ые

---
+++ Task5.0-003/Program.cs
@@ -0,0 +1,24 @@
+﻿//замена положительных цифр на отрицательные и наоборот
+int size = 4;
+int minValue = -9;
+int maxValue = 9;
+int[] array = new int[size];
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(minValue, maxValue + 1);
+}
+
+Console.WriteLine(string.Join(",", array));
+
+// foreach (int el in array)
+// {
+//     Console.Write($"{el * (-1)},");
+// }
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = array[i] * -1;
+}
+
+Console.WriteLine(string.Join(",", array));
