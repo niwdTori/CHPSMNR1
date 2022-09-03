d393f071c0ee54e8bbb44b3343392f8c67400d63
Tori <mes.viktoria@mail.ru>
2022-09-04 01:26:23 +0500

    Присутствует ли данное число в массиве

---
+++ Task5.0-004/Program.cs
@@ -0,0 +1,33 @@
+﻿int size = 5;
+int minValue = -100;
+int maxValue = 100;
+int[] array = new int[size];
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(minValue, maxValue + 1);
+}
+string concat = string.Join(",", array);
+Console.WriteLine(concat);
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+bool exists = false;
+
+for (int i = 0; i < array.Length; i++)
+{
+   if(array[i] == number)
+   {
+    exists = true;
+    break;
+   }
+}
+
+if(exists)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
