9779082e705b26babdce2efb4b1176eee1f71094
Tori <mes.viktoria@mail.ru>
2022-09-04 04:03:05 +0500

    Произведения крайних элементов массива

---
+++ Task5.0-006/Program.cs
@@ -0,0 +1,45 @@
+﻿int size = 3;
+int minValue = 1;
+int maxValue = 10;
+int[] array = new int[size];
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(minValue, maxValue + 1);
+}
+
+Console.WriteLine(string.Join(",", array));
+
+int SizeMas(int size)
+{
+    int sizeMas = 0;
+    int x = size%2;
+    if(x==0)
+    {
+        sizeMas = size/2;
+    }
+    else
+    {
+        sizeMas = size/2+1;
+    }
+    return sizeMas;
+}
+
+int j = array.Length - 1;
+int[] mas = new int[SizeMas(size)];
+int a = 0;
+int count = 0;
+for(int i = 0; i < j; i++)
+{
+    mas[a] = array[i] * array[j];
+    j--;
+    a++;
+    count++;
+
+}
+if(SizeMas(size)!= count)
+{
+    mas[a] = array[a];
+}
+
+Console.WriteLine(string.Join(",", mas));
