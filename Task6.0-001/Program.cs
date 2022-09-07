46a701e591369390a6371b73483395f1593ad267
Tori <mes.viktoria@mail.ru>
2022-09-08 02:24:13 +0500

    Перевернуть массив

---
+++ Task6.0-001/Program.cs
@@ -0,0 +1,37 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]}, ");
+        position++;
+    }
+}    
+
+
+int[] array = new int[4];
+FillArray(array);
+PrintArray(array);
+
+int j = array.Length - 1;
+for(int i = 0; i < j; i++) // i < array.Length - 1
+{
+    int temparray = array[i];
+    array[i] = array[j];  // = array[array.Length - i - 1]
+    array[j] = temprarray; // array[array.Length - i - 1] =
+    j--;
+}
+Console.WriteLine();
+Console.WriteLine(string.Join(", ", array));
