96bb536b6ef1b78fd844f00ec88a84800a56d44a
Tori <mes.viktoria@mail.ru>
2022-09-08 02:33:41 +0500

    Может ли существовать треугольник

---
+++ Task6.0-002/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите сторону a: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите сторону b: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите сторону c: ");
+double c = double.Parse(Console.ReadLine());
+
+if( a + b > c && b+c > a && a+c > b) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
