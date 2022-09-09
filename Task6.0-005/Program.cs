87ccb83c092845b38bebd87928981340878dd553
Tori <mes.viktoria@mail.ru>
2022-09-10 04:26:42 +0500

    Копия заданного массива

---
+++ Task6.0-005/Program.cs
@@ -0,0 +1,23 @@
+﻿// Копия заданого массива
+int[] array = { 1, 3, 4, 8, 1, 8 };
+int[] massiv = new int[array.Length];
+
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    massiv[j] = array[i];
+    j++;
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+PrintArray(array);
+Console.WriteLine();
+PrintArray(massiv);
