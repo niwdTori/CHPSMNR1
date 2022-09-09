b926cb59883f14328f0462a71be740d636658b87
Tori <mes.viktoria@mail.ru>
2022-09-10 04:25:50 +0500

    Первые n чисел фибоначчи

---
+++ Task6.0-004/Program.cs
@@ -0,0 +1,15 @@
+﻿// Первые N чисел фибоначчи
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+int num1 = 0;
+int num2 = 1;
+Console.Write("01");
+for ( int i = 0; i < n-2; i++)
+{
+    int num = num1 + num2;
+    num1 = num2;
+    num2 = num;
+    Console.Write(num);
+}
