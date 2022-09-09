10798cf2f6ce26cbd1bd54f002cd21a5df3f399e
Tori <mes.viktoria@mail.ru>
2022-09-10 03:02:15 +0500

    Десятичное число в двоичное

---
+++ Task6.0-003/Program.cs
@@ -0,0 +1,16 @@
+﻿// Преобразовать десятичное число в двоичное
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int a = 0;
+string b = "";
+
+while(number >= 1)
+{
+    a = number%2;
+    b = a + b;
+    number = number/2;
+}
+
+Console.WriteLine(b);
