274f1e19c9a5273b86503b63cfe3edd6e475097e
Tori <mes.viktoria@mail.ru>
2022-09-10 22:51:04 +0500

    Значения элементов с четными индексами заменяются на квадрат

---
+++ Task7.0-003/Program.cs
@@ -0,0 +1,48 @@
+﻿int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] res = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            res[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return res;
+}
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] NewGetArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                arr[i, j] = arr[i, j] * arr[i, j];
+            }
+        }
+    }
+    return arr;
+}
+
+Console.WriteLine("Введите колличество строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите колличество столбцов: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+int[,] arrayNew = NewGetArray(array);
+PrintArray(arrayNew);
