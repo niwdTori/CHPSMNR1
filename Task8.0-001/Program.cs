a46a331e9716edf75d29bb4c54c11b7c55189783
Tori <mes.viktoria@mail.ru>
2022-09-14 00:00:25 +0500

    Меняет местами первую и последнюю строку массива

---
+++ Task8.0-001/Program.cs
@@ -0,0 +1,46 @@
+﻿//Меняет местами первую и последнюю строку массива
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] res = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            res[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return res;
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите колличество строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите колличество столбцов: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+
+int i = 0;
+int size = array.GetLength(0);
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    int temp = array[i, j];
+    array[i, j] = array[(size-1), j];
+    array[(size-1), j] = temp;
+}
+
+PrintArray(array);
