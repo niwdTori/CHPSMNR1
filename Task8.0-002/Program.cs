9c34edc52fac4350f8ef8d344af75393f880bc8c
Tori <mes.viktoria@mail.ru>
2022-09-14 00:01:28 +0500

    заменяет строки на столбцы

---
+++ Task8.0-002/Program.cs
@@ -0,0 +1,56 @@
+﻿//заменяет строки на столбцы, если невозможно вывести сообщение.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] res = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            res[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return res;
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeArray(int[,] mas)
+{
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            Console.Write($"{mas[j, i]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите колличество строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите колличество столбцов: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+
+if(rows != columns)
+{
+    Console.WriteLine("Невозможно выполнить операцию! Количество строк должно быть равно количеству столбцов");
+}
+else 
+{
+    ChangeArray(array);
+}
